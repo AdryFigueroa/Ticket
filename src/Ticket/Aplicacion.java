a5c314a2372dfcb3aed365fdb88c0157e6e066ad
AdryFigueroa <100029239+AdryFigueroa@users.noreply.github.com>
2022-03-19 21:11:14 -0300

    Add files via upload

---
+++ src/Ticket/Aplicacion.java
@@ -0,0 +1,17 @@
+package Ticket;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Aplicacion {
+	
+	private static ArrayList<Cliente> clientes;
+	private static ArrayList<Ejecutivo> ejecutivos;
+	private static ArrayList<Producto> productos;
+	private static ArrayList<Ticket> tickets;
+
+	public static void main(String[] args) {
+		Scanner consola = new Scanner( System.in);
+
+	}
+}
