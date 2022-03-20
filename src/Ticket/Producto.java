5b8345ba6d9cbc32d322347705e04f134e76845d
AdryFigueroa <100029239+AdryFigueroa@users.noreply.github.com>
2022-03-19 22:15:18 -0300

    Add files via upload

--- src/Ticket/Producto.java
+++ src/Ticket/Producto.java
@@ -1,5 +1,42 @@
 package Ticket;
 
 public class Producto {
+	private int id;
+	private String nombre;
+	private String tipo;
+	
+	public Producto() {
+		
+	}
+	
+	public Producto(int id,String nombre, String tipo) {
+		this.id = id;
+		this.nombre = nombre;
+		this.tipo = tipo; // tipo de producto hogar  movil
+	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	
 }
